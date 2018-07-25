bc8e374cb3aef8df41cdb9a2757e7e2b751198e5
Paia Thao <paiathao09@gmail.com>
2018-07-25 10:04:20 -0500

    currying all clickHandlers

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -9,31 +9,41 @@ class Star extends Component {
             stars: {
                 name: '',
                 role: '',
+                popularity: '',
             }
         }
     }
 
-    handleNameChange = (event) => {
-        console.log(event.target.value);
-
+    handleChangeFor = (propertyName) => (event) => {
         this.setState({
             stars: {
                 ...this.state.stars,
-                name: event.target.value,
+                [propertyName]: event.target.value,
             }
         })
     }
 
-    handleRoleChange = (event) => {
-        console.log(event.target.value);
+    // handleNameChange = (event) => {
+    //     console.log(event.target.value);
 
-        this.setState({
-            stars: {
-                ...this.state.stars,
-                role: event.target.value
-            }
-        })
-    }
+    //     this.setState({
+    //         stars: {
+    //             ...this.state.stars,
+    //             name: event.target.value,
+    //         }
+    //     })
+    // }
+
+    // handleRoleChange = (event) => {
+    //     console.log(event.target.value);
+
+    //     this.setState({
+    //         stars: {
+    //             ...this.state.stars,
+    //             role: event.target.value
+    //         }
+    //     })
+    // }
 
     handleClick = () => {
         console.log(this.state.stars)
@@ -43,8 +53,9 @@ class Star extends Component {
         return (
             <div>
                 <p> {this.state.stars.name} is Famous for {this.state.stars.role} </p>
-                <input type="text" onChange={this.handleNameChange} />
-                <input type="text" onChange={this.handleRoleChange} />
+                <input type="text" onChange={this.handleChangeFor('name')} />
+                <input type="text" onChange={this.handleChangeFor('role')} />
+                <input type="text" onChange={this.handleChangeFor('popularity')} />
                 <button onClick={this.handleClick}>Submit</button>
             </div>
         )
